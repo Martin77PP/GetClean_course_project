6515a75bfb1b064bb03b592582841f64afd38d63
Martin77PP <m.piraino@pvalue-research.com>
2014-10-26 23:05:03 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,4 @@
-setwd("C:/Users/eli e matti/Google Drive/pvalue training/Getting and Cleaning Data/course_project")
-
+setwd("C...")
 
 library("reshape2")
 
@@ -12,20 +11,18 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 features <- read.table("./UCI_HAR_Dataset/features.txt")
 activity_labels <- read.table("./UCI_HAR_Dataset/activity_labels.txt")
 
-
-VAR_names <- c("subject_id", "activity_id", as.character(features$V2))
-
 test_df <- cbind(subject_test, y_test, x_test)
 train_df <- cbind(subject_train, y_train, x_train)
 step1_df <- rbind(test_df, train_df)
+
+VAR_names <- c("subject_id", "activity_id", as.character(features$V2))
 colnames(step1_df) <- VAR_names
 
 mean1 <- grep("mean\\(\\)", names(step1_df), ignore.case = T)
 std1 <- grep("std\\(\\)", names(step1_df), ignore.case = T)
 step2_df <- subset(step1_df, select=c(1,2,mean1,std1))
-step2_df[,2] <- factor(step2_df[,2],labels=activity_labels$V2)
 
-table(step2_df$activity_id)
+step2_df[,2] <- factor(step2_df[,2],labels=activity_labels$V2)
 
 old_name <- c("tBody","Acc","tGravity","Gyro","Mag","f") 
 new_name <- c("time.Body.","Acceleration.", "time.Gravity.", "Angular.velocity.",
